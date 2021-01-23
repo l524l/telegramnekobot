81c50bf466a53a4de3933493a2c90ecf56b9a422
l524l <65085630+l524l@users.noreply.github.com>
2021-01-23 10:52:57 +0300

    удалена(временно) работа с файлом настроек

--- src/main/java/com/github/l524l/telegramnekobot/settings/ProgramSettingsFileConfig.java
+++ src/main/java/com/github/l524l/telegramnekobot/settings/ProgramSettingsFileConfig.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @Configuration
@@ -18,23 +16,12 @@ public class ProgramSettingsFileConfig {
     @Bean
     public ProgramSettings getProgramSettings(){
         ProgramSettings settings;
-        File file = new File("./settings.json");
+        //File file = new File("./settings.json");
         ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            if (!file.exists()){
-                file.createNewFile();
-                settings = new ProgramSettings();
-                settings.setOwner(owner);
-                settings.setWorkMode(WorkMode.LIGHT);
-                settings.setAdminList(new ArrayList<>());
-                objectMapper.writeValue(file,settings);
-                return settings;
-            }else {
-                return objectMapper.readValue(file,ProgramSettings.class);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        settings = new ProgramSettings();
+        settings.setOwner(owner);
+        settings.setWorkMode(WorkMode.LIGHT);
+        settings.setAdminList(new ArrayList<>());
+        return settings;
     }
 }
