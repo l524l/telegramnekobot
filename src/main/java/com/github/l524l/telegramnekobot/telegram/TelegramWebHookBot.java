051870d69df3de174a334f17a96e1e12e8a85bab
l524l <65085630+l524l@users.noreply.github.com>
2021-01-23 12:59:08 +0300

    Метод execute у Command теперь void Убраны костыли

--- src/main/java/com/github/l524l/telegramnekobot/telegram/TelegramWebHookBot.java
+++ src/main/java/com/github/l524l/telegramnekobot/telegram/TelegramWebHookBot.java
@@ -1,19 +1,14 @@
 package com.github.l524l.telegramnekobot.telegram;
 
 import com.github.l524l.telegramnekobot.settings.ProgramSettings;
-import com.github.l524l.telegramnekobot.telegram.apimethod.SendPhotoByURL;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.io.File;
 
 @Component
 public class TelegramWebHookBot extends TelegramWebhookBot {
@@ -45,9 +40,6 @@ public class TelegramWebHookBot extends TelegramWebhookBot {
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        SendPhotoByURL sendPhoto = new SendPhotoByURL();
-        sendPhoto.setChat_id(update.getMessage().getChatId().toString());
-        sendPhoto.setPhoto("https://cdn.nekos.life/neko/neko_070.jpg");
         SendPhoto sendPhoto1 = new SendPhoto();
         sendPhoto1.setChatId(update.getMessage().getChatId().toString());
         sendPhoto1.setPhoto(new InputFile("https://cdn.nekos.life/neko/neko_070.jpg"));
@@ -56,7 +48,7 @@ public class TelegramWebHookBot extends TelegramWebhookBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-        return sendPhoto;
+        return null;
     }
 
     @Override
--- src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Photo.java
+++ src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Photo.java
@@ -1,8 +1,6 @@
 package com.github.l524l.telegramnekobot.telegram.commands;
 
 import com.github.l524l.telegramnekobot.telegram.UserRoles;
-import com.github.l524l.telegramnekobot.telegram.apimethod.SendPhotoByURL;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class Photo extends Command {
@@ -15,10 +13,7 @@ public class Photo extends Command {
     }
 
     @Override
-    public BotApiMethod execute(Update update) {
-        SendPhotoByURL sendPhoto = new SendPhotoByURL();
-        sendPhoto.setChat_id(update.getMessage().getChatId().toString());
-        sendPhoto.setPhoto("https://cdn.nekos.life/neko/neko_070.jpg");
-        return sendPhoto;
+    public void execute(Update update) {
+
     }
 }
