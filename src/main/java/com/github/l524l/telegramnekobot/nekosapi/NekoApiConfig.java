c07ffcab02841b8c856bf0d621672e9b7eda31ed
l524l <65085630+l524l@users.noreply.github.com>
2021-01-24 13:11:57 +0300

    настроено для работы на сервере

--- src/main/java/com/github/l524l/telegramnekobot/nekosapi/NekoApiConfig.java
+++ src/main/java/com/github/l524l/telegramnekobot/nekosapi/NekoApiConfig.java
@@ -1,14 +1,43 @@
 package com.github.l524l.telegramnekobot.nekosapi;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class NekoApiConfig {
     @Bean
     public NekosApi createNekoApi(){
-
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<NekoCategory> list = null;
+        list = new ArrayList<>();
+        list.add(new NekoCategory("AS",true));
+        NekosApi nekosApi = null;
+        try {
+            String s = objectMapper.writeValueAsString(list);
+            ClassPathResource urlResource = new ClassPathResource("./nekocategories.json");
+            list = objectMapper.readValue(urlResource.getInputStream(), new TypeReference<List<NekoCategory>>(){});
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new NekosApi(list);
     }
 }
 
