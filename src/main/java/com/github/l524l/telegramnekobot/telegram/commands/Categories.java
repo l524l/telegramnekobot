76196eff66aaa48b01bbedbaa9ce897142bc2a5a
l524l <65085630+l524l@users.noreply.github.com>
2021-02-01 22:02:45 +0300

    Добавлены все категории. И предупреждение об отсутствии категории

--- src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Categories.java
+++ src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Categories.java
@@ -18,31 +18,29 @@ public class Categories extends Command {
     public void execute(Update update) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(update.getMessage().getChatId().toString());
-        sendMessage.setText("Категории:\n" +
-                "nsfw       | sfw\n" +
-                "-------------------\n"+
-                "4k         | neko\n" +
-                "bellevid   | kiss\n" +
-                "cum        | lick\n" +
-                "gif        | hug\n" +
-                "spank      |\n" +
-                "anal       | tickle\n" +
-                "bj         | wallpapers\n" +
-                "feed       |\n" +
-                "hentai     | baka\n" +
-                "lesbian    |\n" +
-                "poke       |\n" +
-                "animalears |\n" +
-                "blowjob    | laugh\n" +
-                "feet       |\n" +
-                "holo       | foxgirl\n" +
-                "lewd       | waifu\n" +
-                "pussy      | cuddle\n" +
-                "boobs      | pat\n" +
-                "slap       |\n" +
-                "belle      |\n" +
-                "gasm       | smug"
+        sendMessage.setText("```\n" +
+                "nsfw    | sfw\n" +
+                "---------------------\n"+
+                "4k      | neko\n" +
+                "gasm    | kiss\n" +
+                "cum     | lick\n" +
+                "gif     | hug\n" +
+                "lewd    | wallpapers\n" +
+                "anal    | tickle\n" +
+                "hentai  | baka\n" +
+                "lesbian | animalears\n" +
+                "blowjob | laugh\n" +
+                "holo    | feet\n" +
+                "pussy   | cuddle\n" +
+                "boobs   | pat\n" +
+                "        | feed\n" +
+                "        | foxgirl\n" +
+                "        | slap\n" +
+                "        | poke\n" +
+                "        | smug\n```"
         );
+
+        sendMessage.setParseMode("MarkdownV2");
         try {
             telegramSender.execute(sendMessage);
         } catch (TelegramApiException e) {
--- src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Photo.java
+++ src/main/java/com/github/l524l/telegramnekobot/telegram/commands/Photo.java
@@ -7,6 +7,7 @@ import com.github.l524l.telegramnekobot.telegram.UserRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -25,25 +26,32 @@ public class Photo extends Command {
 
     @Override
     public void execute(Update update) {
+        String chat_id = update.getMessage().getChatId().toString();
         try {
             String[] s = update.getMessage().getText().split(" ");
-            String url = nekosApi.execute(s[1]);
+            if (s.length >= 2) {
+                String url = nekosApi.execute(s[1]);
+                try {
+                    if (url.endsWith(".gif")) {
+                        SendDocument sendDocument = new SendDocument();
+                        sendDocument.setChatId(chat_id);
+                        sendDocument.setDocument(new InputFile(url));
 
-            try {
-                if (url.endsWith(".gif")){
-                    SendDocument sendDocument = new SendDocument();
-                    sendDocument.setChatId(update.getMessage().getChatId().toString());
-                    sendDocument.setDocument(new InputFile(url));
-
-                    telegramSender.execute(sendDocument);
-                }else {
-                    SendPhoto sendPhoto = new SendPhoto();
-                    sendPhoto.setChatId(update.getMessage().getChatId().toString());
-                    sendPhoto.setPhoto(new InputFile(url));
-                    telegramSender.execute(sendPhoto);
+                        telegramSender.execute(sendDocument);
+                    } else {
+                        SendPhoto sendPhoto = new SendPhoto();
+                        sendPhoto.setChatId(chat_id);
+                        sendPhoto.setPhoto(new InputFile(url));
+                        telegramSender.execute(sendPhoto);
+                    }
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
                 }
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            } else {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(chat_id);
+                sendMessage.setText("Вы не выбрали категорию.\nВведите /categories для отображения списка категорий");
+                telegramSender.execute(sendMessage);
             }
         }catch (Throwable e){
             e.printStackTrace();
