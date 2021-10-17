a1f3591e29df5c028144d14e5c3eb194e2f24c8e
l524l <65085630+l524l@users.noreply.github.com>
2021-10-17 17:05:23 +0300

    Добавлена совместимость с прод сервером

--- src/main/java/com/github/l524l/telegramnekobot/controllers/WebHookController.java
+++ src/main/java/com/github/l524l/telegramnekobot/controllers/WebHookController.java
@@ -3,10 +3,7 @@ package com.github.l524l.telegramnekobot.controllers;
 import com.github.l524l.telegramnekobot.handlers.UpdateHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
@@ -18,8 +15,12 @@ public class WebHookController {
         this.updateHandler = updateHandler;
     }
 
-    @RequestMapping(path = "/")
+    @RequestMapping(path = "/", method = RequestMethod.POST)
     public @ResponseBody Object getWebHooks(@RequestBody Update update){
         return updateHandler.handleRequest(update);
+    }
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public @ResponseBody Object getWebHooks(){
+        return "ok";
     }
 }
