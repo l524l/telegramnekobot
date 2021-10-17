6a1e9efe545c555fd11c070a7232311c54260079
l524l <65085630+l524l@users.noreply.github.com>
2021-10-17 19:46:29 +0300

    Добавлен банер удален redis и BotProperty

--- src/main/java/com/github/l524l/telegramnekobot/repositories/BotPropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.l524l.telegramnekobot.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BotPropertyRepository extends CrudRepository<String,String> {
-}
