db8371effb8b34614b84c3bb18011934a9191516
l524l <65085630+l524l@users.noreply.github.com>
2021-10-23 19:10:13 +0300

    Теперь есть поддержка tomcat9 и деплой war

--- src/main/java/com/github/l524l/telegramnekobot/TelegramnekobotApplication.java
+++ src/main/java/com/github/l524l/telegramnekobot/TelegramnekobotApplication.java
@@ -2,12 +2,18 @@ package com.github.l524l.telegramnekobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TelegramnekobotApplication {
+public class TelegramnekobotApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramnekobotApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TelegramnekobotApplication.class);
+	}
 }
